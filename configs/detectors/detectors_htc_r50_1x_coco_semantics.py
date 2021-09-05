a16a228bcda516474b478717119ef16e363106dd
Victor Lundgren <victor15_toic@hotmail.com>
2021-09-05 11:05:13 -0300

    Create detectors_htc_r50_1x_coco_semantics.py

---
+++ configs/detectors/detectors_htc_r50_1x_coco_semantics.py
@@ -0,0 +1,42 @@
+# the new config inherits the base configs to highlight the necessary modification
+_base_ = './detectors_htc_r50_1x_coco.py'
+
+# 1. dataset settings
+dataset_type = 'CocoSemanticDataset'
+classes = ('pole', 'cuboid', 'flat', 'disk', 'cylinder', 'sphere', 'wedge')
+data = dict(
+    train=dict(
+        type=dataset_type,
+        # explicitly add your class names to the field `classes`
+        classes=classes),
+    val=dict(
+        type=dataset_type,
+        # explicitly add your class names to the field `classes`
+        classes=classes),
+    test=dict(
+        type=dataset_type,
+        # explicitly add your class names to the field `classes`
+        classes=classes))
+
+# 2. model settings
+# explicitly over-write all the `num_classes` field from default 80 to 7.
+model = dict(
+    roi_head=dict(
+        bbox_head=[
+            dict(
+                type='Shared2FCBBoxHead',
+                # explicitly over-write all the `num_classes` field from default 80 to 7.
+                num_classes=7),
+            dict(
+                type='Shared2FCBBoxHead',
+                # explicitly over-write all the `num_classes` field from default 80 to 7.
+                num_classes=7),
+            dict(
+                type='Shared2FCBBoxHead',
+                # explicitly over-write all the `num_classes` field from default 80 to 7.
+                num_classes=7)],
+    # explicitly over-write all the `num_classes` field from default 80 to 7.
+    ))
+lr_config = dict(
+  policy='step',
+)
